1ac39dc2a4d6a1e9ab9a8ce20b818998fcd06f82
Juan Rodriguez <juan109rodriguez@accenture.com>
2025-09-01 19:09:39 -0500

    clean up server file

--- project starter code/server.js
+++ project starter code/server.js
@@ -11,24 +11,6 @@ const port = process.env.PORT || 8082;
 // Use the body parser middleware for post requests
 app.use(bodyParser.json());
 
-// @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-// GET /filteredimage?image_url={{URL}}
-// endpoint to filter an image from a public url.
-// IT SHOULD
-//    1
-//    1. validate the image_url query
-//    2. call filterImageFromURL(image_url) to filter the image
-//    3. send the resulting file in the response
-//    4. deletes any files on the server on finish of the response
-// QUERY PARAMATERS
-//    image_url: URL of a publicly accessible image
-// RETURNS
-//   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-/**************************************************************************** */
-
-//! END @TODO1
-
 // Root Endpoint
 // Displays a simple message to the user
 app.get("/", async (req, res) => {
@@ -37,7 +19,6 @@ app.get("/", async (req, res) => {
 
 app.get("/filteredimage", async (req, res) => {
   const { image_url } = req.query;
-  console.log(image_url);
   if (!image_url) {
     return res.status(400).send({ message: "image_url is required" });
   }
@@ -52,7 +33,6 @@ app.get("/filteredimage", async (req, res) => {
       }
     });
   } catch (error) {
-    console.log(req.query.image_url, error);
     res
       .status(422)
       .send({ message: "Unable to process the image at the provided URL" });
